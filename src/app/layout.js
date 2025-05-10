96f8882595016098679e04cc88dfe6b6b87a65da
yoohanjin <hanjin3029@gmail.com>
2025-05-10 16:55:51 +0900

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,14 @@
+import { warnOptionHasBeenDeprecated } from "next/dist/server/config";
+
 // app/layout.js
 export const metadata = {
     title: 'Lotto App',
     description: 'A simple Lotto number generator app',
+
+    other: {
+      'google-site-verification':'<meta name="google-site-verification" content="725UNBR54P2G55WwD53J7CTu5ngwcFYblgp2LVYl9n8" /> ',
+      
+    },
   };
   
   export default function RootLayout({ children }) {
